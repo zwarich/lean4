6e49116204138249184e3f1eb391144b03f2a340
Markus Himmel <markus@lean-fro.org>
2024-06-14 14:07:29 +0200

    Final state before giving up

--- src/Init/TacticsExtra.lean
+++ src/Init/TacticsExtra.lean
@@ -22,7 +22,7 @@ private def expandIfThenElse
       pure (← mkName, #[])
     else
       let hole ← withFreshMacroScope mkName
-      let holeId := hole.raw[1]
+      let holeId : Syntax := hole.raw[1]
       let case ← (open TSyntax.Compat in `(tactic|
           case $holeId:ident =>%$tk
             -- annotate `then/else` with state after `case`
