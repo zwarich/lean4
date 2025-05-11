ec7230927c5f3fcf1353ee4b9e7ec6431aaa210f
Cameron Zwarich <cameron@lean-fro.org>
2025-05-11 13:12:52 -0700

    wip change

--- src/Lean/Compiler/LCNF/Closure.lean
+++ src/Lean/Compiler/LCNF/Closure.lean
@@ -34,41 +34,24 @@ structure Context where
   -/
   isUnderBinder : Bool
 
+inductive Value where
+  | nonAbstract (decl : CodeDecl)
+  | abstract (param : Param)
+  deriving Inhabited
+
 /--
 State for the `ClosureM` monad.
 -/
 structure State where
   /--
-  Set of already visited free variables.
+  Values of already visited free variables.
   -/
-  visited : FVarIdSet := {}
-  /--
-  Free variables that must become new parameters of the code being specialized.
-  -/
-  params  : Array Param := #[]
-  /--
-  Let-declarations and local function declarations that are going to be "copied" to the code
-  being processed. For example, when this module is used in the code specializer, the let-declarations
-  often contain the instance values. In the current specialization heuristic all let-declarations are ground values
-  (i.e., they do not contain free-variables).
-  However, local function declarations may contain free variables.
-
-  All customers of this module try to avoid work duplication. If a let-declaration is a ground value,
-  it most likely will be computed during compilation time, and work duplication is not an issue.
-  -/
-  decls   : Array CodeDecl := #[]
+  values : Std.HashMap FVarId Value := {}
 
 /--
 Monad for implementing the dependency collector.
 -/
 abbrev ClosureM := ReaderT Context $ StateRefT State CompilerM
-
-/--
-Mark a free variable as already visited.
-We perform a topological sort over the dependencies.
--/
-def markVisited (fvarId : FVarId) : ClosureM Unit :=
-  modify fun s => { s with visited := s.visited.insert fvarId }
 
 mutual
  /--
@@ -126,29 +109,35 @@ mutual
   If it has not already been visited and is in scope, we collect its dependencies.
   -/
   partial def collectFVar (fvarId : FVarId) : ClosureM Unit := do
-    unless (← get).visited.contains fvarId do
-      markVisited fvarId
+    let processFvar (_ : Unit) : ClosureM Unit := do
       let ctx ← read
       if ctx.inScope fvarId then
         /- We only collect the variables in the scope of the function application being specialized. -/
-        if let some funDecl ← findFunDecl? fvarId then
+        let value ← if let some funDecl ← findFunDecl? fvarId then
           if ctx.isUnderBinder || ctx.abstract funDecl.fvarId then
-            modify fun s => { s with params := s.params.push <| { funDecl with borrow := false } }
+            pure <| .abstract { funDecl with borrow := false }
           else
             collectFunDecl funDecl
-            modify fun s => { s with decls := s.decls.push <| .fun funDecl }
+            pure <| .nonAbstract (.fun funDecl)
         else if let some param ← findParam? fvarId then
           collectType param.type
-          modify fun s => { s with params := s.params.push param }
+          pure <| .abstract param
         else if let some letDecl ← findLetDecl? fvarId then
           collectType letDecl.type
           if ctx.isUnderBinder || ctx.abstract letDecl.fvarId then
-            modify fun s => { s with params := s.params.push <| { letDecl with borrow := false } }
+            pure <| .abstract { letDecl with borrow := false }
           else
             collectLetValue letDecl.value
-            modify fun s => { s with decls := s.decls.push <| .let letDecl }
+            pure <| .nonAbstract (.let letDecl)
         else
           unreachable!
+        modify fun s => { s with values := s.values.insert fvarId value }
+    match (← get).values.get? fvarId with
+    | none => processFvar ()
+    | some (.nonAbstract _) =>
+      if (← read).isUnderBinder then
+        processFvar ()
+    | some (.abstract _) => return ()
 
   /-- Collect dependencies of the given expression. -/
   partial def collectType (type : Expr) : ClosureM Unit := do
@@ -158,7 +147,13 @@ end
 
 def run (x : ClosureM α) (inScope : FVarId → Bool) (abstract : FVarId → Bool := fun _ => true) : CompilerM (α × Array Param × Array CodeDecl) := do
   let (a, s) ← x { inScope, abstract, isUnderBinder := false } |>.run {}
-  return (a, s.params, s.decls)
+  let mut params := #[]
+  let mut decls := #[]
+  for ⟨_, value⟩ in s.values do
+    match value with
+    | .nonAbstract decl => decls := decls.push decl
+    | .abstract param => params := params.push param
+  return (a, params, decls)
 
 end Closure
 
